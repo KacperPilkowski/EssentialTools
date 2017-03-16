2819cfd7cf05f9f3375c217399e27f8e8a7f787e
Kacper <kacperpilkowksi@gmail.com>
2017-03-16 17:48:28 +0100

    shoppingcart and product modal addet

---
+++ EssentialTools/Controllers/HomeController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using EssentialTools.Models;
+
+namespace EssentialTools.Controllers
+{
+    public class HomeController : Controller
+    {
+        private Product[] products =
+        {
+            new Product {Name = "Kajak", Category = "Sporty wodne", Price = 275M},
+            new Product {Name = "Kamizelak ratunkowa", Category = "Sporty wodne", Price = 48.95M},
+            new Product {Name = "Piłka nożna", Category = "Piłka nożna", Price = 19.50M},
+            new Product {Name = "Flaga narodowa", Category = "Piłka nożna", Price = 34.95M}
+        };
+        
+        public ActionResult Index()
+        {
+            LinqValueCalculator calc = new LinqValueCalculator();
+            ShoppingCart cart = new ShoppingCart(calc) {Products = products};
+            decimal tolalValue = cart.CalculateProductTotal();
+            return View(tolalValue);
+        }
+    }
+}
